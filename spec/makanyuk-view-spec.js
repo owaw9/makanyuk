d6dbb12e20cf774eda24bff11b0e4235605cf4ba
owaw9 <asapbebek3@gmail.com>
2022-10-15 14:46:15 +0700

    jancuk

---
+++ spec/makanyuk-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MakanyukView from '../lib/makanyuk-view';
+
+describe('MakanyukView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
